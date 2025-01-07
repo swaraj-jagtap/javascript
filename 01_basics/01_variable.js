78e25dcae9c3c8196b7285bba76c4525985d953b
swaraj-jagtap <swarajjagtap77@gmail.com>
2025-01-07 15:10:00 +0000

    datatypes done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId =13265
+let accountEmail = "swaraj@gmail.com"
+var accountPassword = "123456"   //prefer not to use var   because of issues in b;ock scope and functional scope
+accountCity ="Jaipur"
+let accountState;
+
+
+// accountId= 2   not allowed
+
+accountEmail="swaa.com"
+accountPassword ="1221212"
+accountCity ="pune"
+
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+ "use strict"; //treat all js code as newer version
+ 
+ //aler(3+3)  //we are using node js not browser so alert has different syntax in nodejs
+
+ let name = "swaraj"
+ let age =18
+ let isLoggedIn = false
+
+ //number=> 2 to power of 53
+//  BigInt
+//  string=>""
+//  boolean=>true / false
+// null=> standalone value
+// undefined => not defined by user 
+// symbol =>unique value
+
+// object 
+
+
+console.log(typeof age)
+console.log(typeof null) // output is object 
+console.log(typeof undefined); //undefined
+
+
+

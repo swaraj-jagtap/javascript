0c580e50a968088c75c3e7d5cb3b3a55bb5d20e7
swaraj-jagtap <swarajjagtap77@gmail.com>
2025-01-07 17:17:03 +0000

    conversions

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,28 @@
+let score = "33acd"
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber);
+
+
+// // "33"=>33
+// // "33abc"=>NaN
+// // true =>1;  false=>0
+
+
+let isLoggedIn="sss"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true  ;0=> false
+// "" =>false
+// "swaraj" => true
+
+let someNumber=33
+let stringNumber=String (someNumber)
+console.log(stringNumber);

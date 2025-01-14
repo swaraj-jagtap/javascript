42a98295f388bd85f878016634e378d11dff97ae
swaraj-jagtap <swarajjagtap77@gmail.com>
2025-01-14 16:27:09 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,25 @@
+// primitive 
+
+// 7 types: String,Num,Bool,null,Undefined , Symbol,BigInt
+
+
+
+// reference type (Non premitive)
+
+// Types :Arrays,Object ,Functions
+
+// Array
+// const heros =["shaktiman","naagraj","doga"]
+
+// object:
+/*
+let my_obj=
+ {
+    name:"Swaraj",
+    age=20,
+  } */
+
+const myFunction = function(){
+console.log("Hello World");
+    
+}
